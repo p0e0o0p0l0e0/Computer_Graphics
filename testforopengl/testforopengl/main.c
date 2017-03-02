a27ec38c73ed9ad9a0f658dad138dc20c5194b60
Linqi Zhang <p0e0o0p0l0e0@sina.com>
2017-03-02 17:38:01 +0800

    4.9 OpenGL顶点数组：创建立方体的六个面。简化复杂的坐标描述和大量的函数调用以及共享点的多次指定。

--- testforopengl/testforopengl/main.c
+++ testforopengl/testforopengl/main.c
@@ -159,7 +159,13 @@ void cubeSegment (void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
     
-    cube();
+//    cube();
+    
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glVertexPointer(3, GL_INT, 0, pt);
+    GLubyte vertIndex [] = {6, 2, 3, 7, 5, 1, 0, 4, 7, 3, 1, 5, 4, 0, 2, 6, 2, 0, 1, 3, 7, 5, 4, 6};
+    glDrawElements(GL_QUADS, 24, GL_UNSIGNED_BYTE, vertIndex);
+    glDisableClientState(GL_VERTEX_ARRAY);
     
     glFlush();
 }
