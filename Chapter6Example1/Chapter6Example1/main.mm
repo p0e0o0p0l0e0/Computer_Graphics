a2f6279294d80dab391040b03629641ea76bdae6
Linqi Zhang <p0e0o0p0l0e0@sina.com>
2017-04-25 15:02:54 +0800

    Chapter 6.1.3 画线算法：Bresenham算法: 斜率<1

--- Chapter6Example1/Chapter6Example1/main.mm
+++ Chapter6Example1/Chapter6Example1/main.mm
@@ -9,6 +9,7 @@
 #include <GLUT/GLUT.h>
 #include <stdlib.h>
 #include <math.h>
+#include <iostream>
 
 GLsizei winWidth = 600, winHeight = 500; // Initial display window size.
 
@@ -29,6 +30,7 @@ void setPixel (int x, int y)
 
 inline int round (const float a) { return int (a + 0.5);}
 
+// line DDA
 void lineDDA (int x0, int y0, int xEnd, int yEnd)
 {
     int dx = xEnd - x0, dy = yEnd - y0, steps, k;
@@ -56,6 +58,45 @@ void lineDDA (int x0, int y0, int xEnd, int yEnd)
     }
 }
 
+//line Bresenham: 斜率slope < 1
+
+void lineBres (int x0, int y0, int xEnd, int yEnd)
+{
+    int dx = fabs(xEnd - x0), dy = fabs(yEnd - y0);
+    int p = 2 * dy - dx;
+    int twoDy = 2 * dy, twoDyMinusDx = 2 * (dy - dx);
+    int x, y;
+    
+    if(x0 > xEnd)
+    {
+        x = xEnd;
+        y = yEnd;
+        xEnd = x0;
+    }
+    else
+    {
+        x = x0;
+        y = y0;
+    }
+    
+    setPixel(x, y);
+    
+    while (x < xEnd) {
+        x++;
+        if (p < 0)
+        {
+            p += twoDy;
+        }
+        else
+        {
+            y++;
+            p += twoDyMinusDx;
+        }
+        setPixel(x, y);
+    }
+    std::cout << x << "," << y << std::endl;
+}
+
 void displayFcn (void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
@@ -63,6 +104,10 @@ void displayFcn (void)
     glPointSize(3.0);
     glColor3f(1.0, 1.0, 0.0);
     lineDDA(1, 1, 80, 30);
+    
+    glPointSize(5.0);
+    glColor3f(0.0, 0.0, 1.0);
+    lineBres(15, 20, 90, 40);
     
     glFlush();
 }
@@ -73,7 +118,7 @@ int main(int argc, char * argv[])
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowPosition(100, 100); // 窗口位置，屏幕左上角为(0,0)，窗口左上角距离屏幕左上角100,100。
     glutInitWindowSize(winWidth, winHeight); // 窗口大小
-    glutCreateWindow("Draw Curves");
+    glutCreateWindow("Draw Lines");
     
     init();
     glutDisplayFunc(displayFcn);
