1056ec28f44554b81f2605f4de7482834f4c6189
Linqi Zhang <p0e0o0p0l0e0@sina.com>
2017-04-25 15:39:45 +0800

    Chapter 6.1 画线算法：满足各种斜率的DDA算法和Bresenham算法

--- Chapter6Example1/Chapter6Example1/main.mm
+++ Chapter6Example1/Chapter6Example1/main.mm
@@ -45,6 +45,12 @@ void lineDDA (int x0, int y0, int xEnd, int yEnd)
         steps = dy;
     }
     
+    if(steps <= 0)
+    {
+        steps = 1;
+    }
+    std::cout << "steps : " << steps << std::endl;
+    
     xIncrement = float(dx) / float(steps);
     yIncrement = float(dy) / float(steps);
     
@@ -58,9 +64,9 @@ void lineDDA (int x0, int y0, int xEnd, int yEnd)
     }
 }
 
-//line Bresenham: 斜率slope < 1
+//line Bresenham: 斜率slope <= 1
 
-void lineBres (int x0, int y0, int xEnd, int yEnd)
+void lineBres1 (int x0, int y0, int xEnd, int yEnd, bool slopepositive = true)
 {
     int dx = fabs(xEnd - x0), dy = fabs(yEnd - y0);
     int p = 2 * dy - dx;
@@ -89,12 +95,85 @@ void lineBres (int x0, int y0, int xEnd, int yEnd)
         }
         else
         {
-            y++;
+            if (slopepositive)
+                y++;
+            else
+                y--;
             p += twoDyMinusDx;
         }
         setPixel(x, y);
     }
     std::cout << x << "," << y << std::endl;
+}
+
+//line Bresenham: 斜率slope > 1
+
+void lineBres2 (int x0, int y0, int xEnd, int yEnd, bool slopepositive = true)
+{
+    int dx = fabs(xEnd - x0), dy = fabs(yEnd - y0);
+    int p = 2 * dx - dy;
+    int twoDx = 2 * dx, twoDxMinusDy = 2 * (dx - dy);
+    int x, y;
+    
+    if(y0 > yEnd)
+    {
+        x = xEnd;
+        y = yEnd;
+        yEnd = y0;
+    }
+    else
+    {
+        x = x0;
+        y = y0;
+    }
+    
+    setPixel(x, y);
+    
+    while (y < yEnd) {
+        y++;
+        if (p < 0)
+        {
+            p += twoDx;
+        }
+        else
+        {
+            if (slopepositive)
+                x++;
+            else
+                x--;
+            p += twoDxMinusDy;
+        }
+        setPixel(x, y);
+    }
+    std::cout << x << "," << y << std::endl;
+}
+
+//line Bresenham
+void lineBres (int x0, int y0, int xEnd, int yEnd)
+{
+    bool slopePositive = true;
+    float slope = 0.0;
+    if(x0 == xEnd)
+    {
+        lineBres2(x0, y0, xEnd, yEnd, slopePositive);
+    }
+    else if(y0 == yEnd)
+    {
+        lineBres1(x0, y0, xEnd, yEnd, slopePositive);
+    }
+    else
+    {
+        slope = float(y0-yEnd)/float(x0-xEnd);
+        slopePositive = slope > 0;
+    }
+    if(fabs(slope) <= 1)
+    {
+        lineBres1(x0, y0, xEnd, yEnd, slopePositive);
+    }
+    else
+    {
+        lineBres2(x0, y0, xEnd, yEnd, slopePositive);
+    }
 }
 
 void displayFcn (void)
@@ -103,11 +182,17 @@ void displayFcn (void)
     
     glPointSize(3.0);
     glColor3f(1.0, 1.0, 0.0);
-    lineDDA(1, 1, 80, 30);
+    lineDDA(1, 1, 80, 30); // 0 < slope < 1
+    lineDDA(1, 1, 1, 30); // slope 无穷大
+    lineDDA(1, 80, 1, 80); // slope = 0
+
     
     glPointSize(5.0);
     glColor3f(0.0, 0.0, 1.0);
-    lineBres(15, 20, 90, 40);
+    lineBres(90, 15, 20, 40); // -1 < slope < 0
+    lineBres(15, 20, 40, 90); // slope > 1
+    lineBres(15, 50, 40, 50); // slope = 0
+    lineBres(15, 30, 15, 80); // slope 无穷大
     
     glFlush();
 }
