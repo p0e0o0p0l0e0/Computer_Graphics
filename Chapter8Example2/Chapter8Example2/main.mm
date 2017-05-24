5fc77930cc37dc1dab52f8c54b829582db6b152b
Linqi Zhang <p0e0o0p0l0e0@sina.com>
2017-05-24 18:35:36 +0800

    Chapter 8.4 OpenGL 二维观察函数 // 管理多个窗口，glutHideWindow() glutPopWindow() glutIconifyWindow() glutSetIconTitle(“”)没有效果，待验证

--- Chapter8Example2/Chapter8Example2/main.mm
+++ Chapter8Example2/Chapter8Example2/main.mm
@@ -77,6 +77,22 @@ void displayFcn2 (void)
     glFlush();
 }
 
+void displayFcn3 (void)
+{
+    wcPt2D verts [3] = {{-50, -25}, {50, -25}, {0.0, 50}};
+    
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    glColor3f(1.0, 1.0, 1.0);
+    line(-100, 0, 100, 0);
+    line(0, -100, 0, 100);
+    
+    glColor3f(0.0, 1.0, 0.0);
+    triangle(verts);
+    
+    glFlush();
+}
+
 int main(int argc, char ** argv)
 {
     glutInit (&argc, argv);
@@ -90,21 +106,32 @@ int main(int argc, char ** argv)
     glutDisplayFunc(displayFcn1);
     std::cout << "windowID1 : " << windowID1 << std::endl;
     
-    glutInitWindowPosition(600, 100);
+    glutInitWindowPosition(400, 100);
     glutInitWindowSize(600, 300);
     GLint windowID2 = glutCreateWindow("Split-Screen Example2");
     init();
     glutDisplayFunc(displayFcn2);
     std::cout << "windowID2 : " << windowID2 << std::endl;
     
-    glutInitWindowPosition(600, 100);
+    glutInitWindowPosition(100, 300);
     glutInitWindowSize(600, 300);
     GLint subWindowID1 = glutCreateWindow("Sub Window Example1");
     init();
-    glutDisplayFunc(displayFcn2);
+    glutDisplayFunc(displayFcn3);
     std::cout << "subWindowID1 : " << subWindowID1 << std::endl;
     
-    glutDestroyWindow(windowID2);
+    glutPushWindow(); // 有效果
+    glutPopWindow(); // 有效果
+    
+    glutHideWindow(); // 没有效果
+    
+    glutSetWindow(windowID1);
+    glutPopWindow(); // 没有效果
+    glutSetWindowTitle("new name display on window1");
+    
+    glutPostRedisplay();
+//    glutIconifyWindow(); // 有飞出的效果，没有图标效果
+//    glutSetIconTitle("new new name display on window1");
     
     glutMainLoop();
 }
