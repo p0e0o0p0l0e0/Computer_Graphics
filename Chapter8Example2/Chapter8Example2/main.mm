884ea2f7cfec751dc7398260412dfe8f7353c4e5
Linqi Zhang <p0e0o0p0l0e0@sina.com>
2017-05-25 12:50:12 +0800

    Chapter 8.4 OpenGL 二维观察函数 // 管理多个窗口，有些无效的情况，暂时发现是对glutSetWindow后的当前窗口无效

--- Chapter8Example2/Chapter8Example2/main.mm
+++ Chapter8Example2/Chapter8Example2/main.mm
@@ -106,37 +106,43 @@ int main(int argc, char ** argv)
     glutDisplayFunc(displayFcn1);
     std::cout << "windowID1 : " << windowID1 << std::endl;
     
+    glutHideWindow(); // 在此有效
+    glutShowWindow(); // 在此有效
+//    glutIconifyWindow(); // 在此有效
+    
     glutInitWindowPosition(400, 100);
     glutInitWindowSize(600, 300);
     GLint windowID2 = glutCreateWindow("Split-Screen Example2");
     init();
     glutDisplayFunc(displayFcn2);
     std::cout << "windowID2 : " << windowID2 << std::endl;
+//    glutFullScreen(); // 不能用在子窗口上，对一级窗口有效
     
     glutInitWindowPosition(100, 300);
     glutInitWindowSize(600, 300);
-    GLint subWindowID1 = glutCreateWindow("Sub Window Example1");
+    GLint subWindowID1 = glutCreateSubWindow(windowID2, 250, 100, 100, 100); // 子窗口的大小和坐标是相对于显示窗口的，600*300的显示窗口的子窗口如果想显示在窗口正中间，距离左下角的坐标应为(250, 100);
     init();
     glutDisplayFunc(displayFcn3);
-    std::cout << "subWindowID1 : " << subWindowID1 << std::endl;
+    std::cout << "subWindowID1 : " << subWindowID1 << std::endl; // subwindow编号是在一级窗口后继续累加
     
-    glutPushWindow(); // 有效果
-    glutPopWindow(); // 有效果
+    glutSetCursor(GLUT_CURSOR_HELP); // 对子窗口和一级窗口都ok
+    GLint posX = glutGet(GLUT_WINDOW_X);
+    std::cout << "current window x : " << posX << std::endl;
     
-    glutHideWindow(); // 没有效果
+//    glutPushWindow(); // 对当前窗口有效
+//    glutPopWindow(); // 对当前窗口有效
+//    glutHideWindow(); // 对于子窗口有效
+//    glutShowWindow(); // 对于子窗口有效
     
+    
+    // 大概总结一下，使用SetWindow设置的当前窗口，pop,push,hide,show,iconify等都没有效果
     glutSetWindow(windowID1);
     glutPopWindow(); // 没有效果
     glutSetWindowTitle("new name display on window1");
     
-    glutPostRedisplay();
+//    glutPostRedisplay(); // 什么时候用？
 //    glutIconifyWindow(); // 有飞出的效果，没有图标效果
-//    glutSetIconTitle("new new name display on window1");
-    
-    glutSetCursor(GLUT_CURSOR_HELP);
-    
-    GLint posX = glutGet(GLUT_WINDOW_X);
-    std::cout << "current window x : " << posX << std::endl;
+//    glutSetIconTitle("new new name display on window1"); // 没有效果
     
     glutMainLoop();
 }
