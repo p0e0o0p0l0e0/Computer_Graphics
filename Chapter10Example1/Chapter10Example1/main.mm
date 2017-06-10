0fc63495494db7f25a0ab0d9ccf918160af1ecdd
Linqi Zhang <p0e0o0p0l0e0@sina.com>
2017-06-10 10:59:37 +0800

    Chapter 10.10.6 OpenGL三维观察程序实例：创建一个立方体100*100*100，将投影观察点（即观察系原点）设置在矩形中心(50, 50, 50)，视点Pref为相机朝向的点，初始设置为(0, 50, 0)。修改Pref的坐标，使其绕x=z=50进行逆时针旋转，实现相机在立方体中逆时针旋转，看到不同面的动画。画立方体使用的是Chapter4.9中的OpenGL顶点数组方法。

--- Chapter10Example1/Chapter10Example1/main.mm
+++ Chapter10Example1/Chapter10Example1/main.mm
@@ -7,24 +7,57 @@
 //
 
 #include <GLUT/GLUT.h>
+#include <math.h>
 
 GLint winWidth = 600, winHeight = 600;
-GLfloat x0 = 100.0, y0 = 50.0, z0 = 50.0;
-GLfloat xref = 50.0, yref = 50.0, zref = 0.0;
+GLfloat x0 = 50.0, y00 = 50.0, z0 = 50.0; // 这是观察参考点，与观察系原点重合
+GLfloat xref = 0.0, yref = 50.0, zref = 0.0; // camera要瞄准的点，不是观察参考点
 GLfloat Vx = 0.0, Vy = 1.0, Vz = 0.0;
-GLfloat xwMin = -40.0, ywMin = -60.0, xwMax = 40.0, ywMax = 60.0;
-GLfloat dnear = 25.0, dfar = 125.0;
+GLfloat xwMin = -40.0, ywMin = -40.0, xwMax = 40.0, ywMax = 40.0;
+GLfloat dnear = 25, dfar = 75;
+GLdouble radianAngle = 1.0/360.0 * 3.14159;
+GLfloat cos1 = cos(radianAngle);
+GLfloat sin1 = sin(radianAngle);
+
+typedef GLint vertex3 [3];
+vertex3 pt [8] = {{0, 0, 0}, {0, 100, 0}, {100, 0, 0}, {100, 100, 0},
+                {0, 0, 100}, {0, 100, 100}, {100, 0, 100}, {100, 100, 100}};
 
 void init (void)
 {
-    
     glClearColor(1.0, 1.0, 1.0, 0.0);
     
     glMatrixMode(GL_MODELVIEW);
-    gluLookAt(x0, y0, z0, xref, yref, zref, Vx, Vy, Vz);
+    gluLookAt(x0, y00, z0, xref, yref, zref, Vx, Vy, Vz);
     
     glMatrixMode(GL_PROJECTION);
     glFrustum(xwMin, xwMax, ywMin, ywMax, dnear, dfar);
+}
+
+void quad (GLint n1, GLint n2, GLint n3, GLint n4)
+{
+    glBegin(GL_QUADS);
+    glVertex3iv(pt[n1]);
+    glVertex3iv(pt[n2]);
+    glVertex3iv(pt[n3]);
+    glVertex3iv(pt[n4]);
+    glEnd();
+}
+
+void cube ()
+{
+    glColor3f(0.0, 1.0, 0.0); // back
+    quad(0, 2, 3, 1);
+    glColor3f(1.0, 0.0, 0.0); // left
+    quad(0, 1, 5, 4);
+    glColor3f(0.0, 0.0, 1.0); // top
+    quad(1, 3, 7, 5);
+    glColor3f(1.0, 1.0, 0.0); // front
+    quad(4, 5, 7, 6);
+    glColor3f(0.0, 1.0, 1.0); // right
+    quad(2, 6, 7, 3);
+    glColor3f(1.0, 0.0, 1.0); // bottom
+    quad(0, 4, 6, 2);
 }
 
 void displayFcn (void)
@@ -35,30 +68,10 @@ void displayFcn (void)
     glPolygonMode(GL_FRONT, GL_FILL);
     glPolygonMode(GL_BACK, GL_LINE);
 //    glFrontFace(GL_CW); // show the back
-    glBegin(GL_QUADS);
-    glVertex3f(0.0, 0.0, 0.0);
-    glVertex3f(100.0, 0.0, 0.0);
-    glVertex3f(100.0, 100.0, 0.0);
-    glVertex3f(0.0, 100.0, 0.0);
-    glEnd();
     
-    glColor3f(1.0, 0.0, 0.0);
-    glBegin(GL_QUADS);
-    glVertex3f(0.0, 0.0, 0.0);
-    glVertex3f(0.0, 100.0, 0.0);
-    glVertex3f(0.0, 100.0, 100.0);
-    glVertex3f(0.0, 0.0, 100.0);
-    glEnd();
+    cube();
     
-    glColor3f(0.0, 0.0, 1.0);
-    glBegin(GL_QUADS);
-    glVertex3f(0.0, 100.0, 0.0);
-    glVertex3f(100.0, 100.0, 0.0);
-    glVertex3f(100.0, 100.0, 100.0);
-    glVertex3f(0.0, 100.0, 100.0);
-    glEnd();
-    
-    glFlush();
+    glutSwapBuffers();
 }
 
 void reshapeFcn (GLint newWidth, GLint newHeight)
@@ -69,10 +82,27 @@ void reshapeFcn (GLint newWidth, GLint newHeight)
     winHeight = newHeight;
 }
 
+void idleFcn (void)
+{
+    GLfloat xref1, zref1;
+    xref = xref - x0;
+    zref = zref - z0;
+    zref1 = zref * cos1 - xref * sin1 + z0;
+    xref1 = zref * sin1 + xref * cos1 + x0;
+    xref = xref1;
+    zref = zref1;
+    
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(x0, y00, z0, xref, yref, zref, Vx, Vy, Vz);
+    
+    displayFcn();
+}
+
 int main(int argc, char * argv[])
 {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
     glutInitWindowSize(winWidth, winHeight);
     glutInitWindowPosition(50, 50);
     glutCreateWindow("Perspective View of A Square");
@@ -80,6 +110,7 @@ int main(int argc, char * argv[])
     init();
     glutDisplayFunc(displayFcn);
     glutReshapeFunc(reshapeFcn);
+    glutIdleFunc(idleFcn);
     glutMainLoop();
     
     return 0;
