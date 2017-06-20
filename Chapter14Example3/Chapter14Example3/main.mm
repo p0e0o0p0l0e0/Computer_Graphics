a8c9fa678710c2a45e4a139584b69ba8867d97df
Linqi Zhang <p0e0o0p0l0e0@sina.com>
2017-06-20 11:30:45 +0800

    Chapter 14.16.2 OpenGL的Bézier样条曲面函数

---
+++ Chapter14Example3/Chapter14Example3/main.mm
@@ -0,0 +1,76 @@
+//
+//  main.mm
+//  Chapter14Example3
+//
+//  Created by 张琳琪 on 2017/6/20.
+//  Copyright © 2017年 张琳琪. All rights reserved.
+//
+
+#include <GLUT/GLUT.h>
+
+GLsizei winWidht = 500, winHeight = 500;
+GLfloat uMin = 1.0, uMax = 20.0;
+
+void init (void)
+{
+    glClearColor(1.0, 1.0, 1.0, 0.0);
+    
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(5.0, 5.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glFrustum(-5.0, 5.0, -5.0, 5.0, 4.0, 10.0);
+}
+
+void displayFcn (void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    GLfloat ctrlPts [4][4][3] = {
+        {{-1.5, -1.5, 4.0}, {-0.5, -1.5, 2.0}, {-0.5, -1.5, -1.0}, {1.5, -1.5, 2.0}},
+        {{-1.5, -0.5, 1.0}, {-0.5, -0.5, 3.0}, {0.5, -0.5, 0.0},   {1.5, -0.5, -1.0}},
+        {{-1.5, 0.5, 4.0},  {-0.5, 0.5, 0.0},  {0.5, 0.5, 3.0},    {1.5, 0.5, 4.0}},
+        {{-1.5, 1.5, -2.0}, {-0.5, 1.5, -2.0}, {0.5, 1.5, 0.0},    {1.5, 1.5, -1.0}} };
+    
+    glMap2f(GL_MAP2_VERTEX_3, 0.0, 1.0, 3, 4, 0.0, 1.0, 12, 4, &ctrlPts[0][0][0]);
+    glEnable(GL_MAP2_VERTEX_3);
+    GLint k, j;
+    glColor3f(0.0, 0.0, 1.0);
+    for (k = 0; k <= 8; k++) {
+        glBegin(GL_LINE_STRIP);
+        for (j = 0; j <= 40; j++) {
+            glEvalCoord2f(GLfloat(j)/40.0, GLfloat(k)/8.0);
+        }
+        glEnd();
+        glBegin(GL_LINE_STRIP);
+        for (j = 0; j <= 40; j++) {
+            glEvalCoord2f(GLfloat(k)/8.0, GLfloat(j)/40.0);
+        }
+        glEnd();
+    }
+    
+    glFlush();
+}
+
+void winReshapeFcn (GLint newWidth, GLint newHeight)
+{
+    glViewport(0, 0, newWidth, newHeight);
+}
+
+int main(int argc, char * argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(winWidht, winHeight);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("OpenGL Bézier Curve Surface");
+    
+    init();
+    glutDisplayFunc(displayFcn);
+    glutReshapeFunc(winReshapeFcn);
+    glutMainLoop();
+    
+    return 0;
+}
