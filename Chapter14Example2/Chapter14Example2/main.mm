f909a0e35c3b775888a4c809c0f1cb8a2e73f9f6
Linqi Zhang <p0e0o0p0l0e0@sina.com>
2017-06-20 10:31:50 +0800

    Chapter 14.16.1 OpenGL的Bézier样条曲线函数

---
+++ Chapter14Example2/Chapter14Example2/main.mm
@@ -0,0 +1,70 @@
+//
+//  main.mm
+//  Chapter14Example2
+//
+//  Created by 张琳琪 on 2017/6/20.
+//  Copyright © 2017年 张琳琪. All rights reserved.
+//
+
+#include <GLUT/GLUT.h>
+
+GLsizei winWidht = 500, winHeight = 500;
+GLfloat uMin = 1.0, uMax = 20.0;
+
+void init (void)
+{
+    glClearColor(1.0, 1.0, 1.0, 0.0);
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(-250, 250, -250, 250);
+}
+
+void displayFcn (void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    GLfloat ctrlPts [4][3] = {{-40.0, -40.0}, {-10.0, 200.0}, {10.0, -200.0}, {40.0, 40.0}};
+    glMap1f(GL_MAP1_VERTEX_3, uMin, uMax, 3, 4, *ctrlPts);
+    glEnable(GL_MAP1_VERTEX_3);
+    
+    GLint k;
+    GLfloat step, uValue;
+    step = (uMax - uMin) / 50;
+    glColor3f(0.0, 0.0, 1.0);
+    glBegin(GL_LINE_STRIP);
+    for (uValue = uMin; uValue <= uMax; uValue += step) {
+        glEvalCoord1f(uValue);
+    }
+    glEnd();
+    
+    glColor3f(1.0, 0.0, 0.0);
+    glPointSize(5.0);
+    glBegin(GL_POINTS);
+    for (k = 0; k < 4; k++) {
+        glVertex3fv(&ctrlPts[k][0]);
+    }
+    glEnd();
+    glDisable(GL_MAP1_VERTEX_3);
+    
+    glFlush();
+}
+
+void winReshapeFcn (GLint newWidth, GLint newHeight)
+{
+    glViewport(0, 0, newWidth, newHeight);
+}
+
+int main(int argc, char * argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(winWidht, winHeight);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("OpenGL Bézier Curve");
+    
+    init();
+    glutDisplayFunc(displayFcn);
+    glutReshapeFunc(winReshapeFcn);
+    glutMainLoop();
+    
+    return 0;
+}
